62ca4c8f415707558347cd89998dbe66038d6af3
andshrew <andrew@andrew-m.co.uk>
2018-12-08 00:37:07 +0000

    Update lolhack.sh

--- Enable PCSX Debug Menu/lolhack.sh
+++ Enable PCSX Debug Menu/lolhack.sh
@@ -1,5 +1,5 @@
 #   gpghax / lolhack script to enable debug menu access
-#		Insert USB stick once the system has booted to the menu. The interface will restart.
+#   Insert USB stick once the system has booted to the menu. The interface will restart.
 #   Press Select + Triangle in-game to access the PCSX menu
 
 #   Set the PCSX_ESC_KEY environment variable
